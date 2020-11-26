1e6d0f4aff4946c7cbcada95ea4e99a275b9a4ee
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-26 14:35:22 +0100

    Initial setup

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from '../../data';
+import logo from './logo.svg';
+
+const Navbar: React.FC = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
